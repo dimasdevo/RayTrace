60328c8292215a8db5be085ba3d5314725b98cb8
dimasdevo <dimasriskahadi01@gmail.com>
2015-10-20 22:36:13 +0700

    UPDATE
    Perubahan Matrix Camera dan lain-lain menggunakan class Point (x,y,z)

--- RayTraceProject/RayTraceProject/Source.cpp
+++ RayTraceProject/RayTraceProject/Source.cpp
@@ -1,59 +1,100 @@
 #include <iostream>
+#include <math.h>
 
 #define VECTOR 3
 #define WIDTH 100
 #define LENGTH 100
 
 using namespace std;
+
+//  ------------------  Class Point ----------------------
+
+class Point{
+private:
+	float x,y,z;
+public:
+	Point();
+	Point(float,float,float);
+	float GetX(){
+		return x;
+	}
+	float GetY(){
+		return y;
+	}
+	float GetZ(){
+		return z;
+	}
+	void SetX(float a){
+		x=a;
+	}
+	void SetY(float b){
+		y=b;
+	}
+	void SetZ(float c){
+		z=c;
+	}
+};
+
+Point::Point(float a, float b, float c){
+	x=a;
+	y=b;
+	z=c;
+}
+Point::Point(){};
+
 //	-------------------	   Class Ray	----------------------------
 class Ray{
 private:
-	float position[4];
+	Point Position;
+	Point Direction;
+	float t_min, t_max;
 public:
-	Ray(float*);
+	Ray(Point);
 	Ray();
-	void setRay(float a[]){
-		for(int i=0; i<VECTOR; i++){
-			position[i]=a[i];
-		}
+	void setRay(Point temp){
+		Position=temp;
 	};
-	float* getRay(){
-		return position;
+	Point getRay(){
+		return Position;
 	};
 };
 
-Ray::Ray(float a[]){
-	for(int i=0; i<VECTOR; i++){
-		position[i]=a[i];
-	}
+Ray::Ray(Point temp){
+	Position=temp;
 }
 
 Ray::Ray(){}
-
-
 //	-------------------	   Class Camera	----------------------------
 class Camera{
 private:
-	float lookfrom[4], lookto[4], up[4];
+	Point lookfrom, lookto, up;
 public:
-	Camera(float*, float*, float*);
+	Camera(Point, Point, Point);
+
 	void print(){
-		for(int i=0; i<VECTOR; i++){
-			printf("%f %f %f\n", lookfrom[i], lookto[i],up[i]);
-		}
+		cout<<lookfrom.GetX()<<" "<<lookfrom.GetY()<<" "<<lookfrom.GetZ()<<'\n';
+		cout<<lookto.GetX()<<" "<<lookto.GetY()<<" "<<lookto.GetZ()<<'\n';
+		cout<<up.GetX()<<" "<<up.GetY()<<" "<<up.GetZ()<<'\n';
 	}
-	float* getEye(){
+
+	Point GetLookFrom(){
 		return lookfrom;
+	}
+
+	Point GetLookTo(){
+		return lookto;
+	}
+
+	Point GetUp(){
+		return up;
 	}
 };
 
 
-Camera::Camera(float a[], float b[], float c[]){
-	for(int i=0; i<VECTOR; i++){
-		lookfrom[i]=a[i];
-		lookto[i]=b[i];
-		up[i]=c[i];
-	}
+Camera::Camera(Point a, Point b, Point c){
+	lookfrom=a;
+	lookto=b;
+	up=c;
 }
 // ----------------- Codingan Part 1 (Camera Position)---------------------------------------------
 
@@ -65,19 +106,42 @@ Camera NormalizeUVW(Camera cam){
 
 
 float GetDirectionA(float fov, int size, int var){
-	float direction=(var-(size/2))/size;
+	float direction=((float)var-(size/2))/size;
 	return direction;
 }
 
 float GetDirectionB(float fov, int size, int var){
-	float direction=((size/2)-var)/size;
+	float direction=((float)(size/2)-var)/size;
 	return direction;
 }
 
 Ray ComputeRay(Camera cam, Camera normCam, float a, float b){
 	Ray temp;
+	float n;
+	Point temp0, temp1[3], temps, tempp;
+	temp0=cam.GetLookFrom();
+	temp1[0]=normCam.GetLookFrom();
+	temp1[1]=normCam.GetLookTo();
+	temp1[2]=normCam.GetUp();
 
-	return temp;
+	temps.SetX(temp1[0].GetX()*a+temp1[1].GetX()*b-temp1[2].GetX());
+	temps.SetY(temp1[0].GetX()*a+temp1[1].GetX()*b-temp1[2].GetX());
+	temps.SetZ(temp1[0].GetX()*a+temp1[1].GetX()*b-temp1[2].GetX());
+
+	n=sqrt((temps.GetX()*temps.GetX())+(temps.GetY()*temps.GetY())+(temps.GetZ()*temps.GetZ()));
+
+	tempp.SetX(temp0.GetX()+(temps.GetX()/n));
+	tempp.SetY(temp0.GetY()+(temps.GetY()/n));
+	tempp.SetZ(temp0.GetZ()+(temps.GetZ()/n));
+
+	cout<<tempp.GetX()<<" "<<tempp.GetY()<<" "<<tempp.GetZ()<<'\n';
+	cout<<temp1[0].GetX()<<" "<<temp1[0].GetY()<<" "<<temp1[0].GetZ()<<'\n';
+	cout<<temp1[1].GetX()<<" "<<temp1[1].GetY()<<" "<<temp1[1].GetZ()<<'\n';
+	cout<<temp1[2].GetX()<<" "<<temp1[2].GetY()<<" "<<temp1[2].GetZ()<<'\n';
+
+
+	system("PAUSE");
+	return temp0;
 }
 
 Ray RayThruPixel(Camera cam, int i, int j){
@@ -85,8 +149,8 @@ Ray RayThruPixel(Camera cam, int i, int j){
 	Camera normCam = NormalizeUVW(cam);
 	normCam.print();
 	system("PAUSE");
-	float a=GetDirectionA(1, WIDTH, j);
-	float b=GetDirectionB(1, LENGTH, i);
+	float a=GetDirectionA(0.5, WIDTH, j);
+	float b=GetDirectionB(0.5, LENGTH, i);
 	temp=ComputeRay(cam,normCam,a,b);
 	return temp;
 }
@@ -104,14 +168,18 @@ void RayTrace(Camera cam, int width, int height){
 //  ---------------------- Main Program.c -----------------------------------
 
 int main(int argc, char *argv[]){
-	float posisiKamera[4][4];
+	Point posisiKamera[4];
+	float a,b,c;
 	cout<<"Masukkan koordinat kamera: \n";
 	for(int i=0; i<VECTOR; i++){
-		for(int j=0; j<VECTOR; j++){
-			cin>> posisiKamera[i][j];
-		}
+		cin>>a>>b>>c;
+		posisiKamera[i].SetX(a);
+		posisiKamera[i].SetY(b);
+		posisiKamera[i].SetZ(c);
 	}
-	Camera A(posisiKamera[0], posisiKamera[1],posisiKamera[2]);
+	Camera A(posisiKamera[0], posisiKamera[1], posisiKamera[2]);
+	A.print();
+	system("PAUSE");
 	RayTrace(A,WIDTH,LENGTH);
 	return 0;
 }

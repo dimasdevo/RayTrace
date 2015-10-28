6b18b5c759086c73261e0ce38eeb5dc28987fe8f
Faishal Azka <azukineru@gmail.com>
2015-10-28 22:35:58 +0700

    Add function transformation

--- RayTraceProject/RayTraceProject/Source.cpp
+++ RayTraceProject/RayTraceProject/Source.cpp
@@ -120,6 +120,64 @@ Matrix NormalizeUVW(Camera cam){
 	return temp;
 }
 
+// ---------------Tranformation Function------------------------- //
+Vector TransformTranslate(Vector a, Vector value){
+    float temp1, temp2, temp3;
+    temp1 = a.GetX() + value.GetX();
+    temp2 = a.GetY() + value.GetY();
+    temp3 = a.GetZ() + value.GetZ();
+
+    Vector res(temp1,temp2,temp3);
+    return res;
+}
+
+Vector TransformScale(Vector a, Vector value){
+    float temp1, temp2, temp3;
+    temp1 = a.GetX() * value.GetX();
+    temp2 = a.GetY() * value.GetY();
+    temp3 = a.GetZ() * value.GetZ();
+
+    Vector res(temp1,temp2,temp3);
+    return res;
+}
+
+Vector TransformRotate(Vector a, Vector center, float degree){
+    float point1, point2, point3;
+    // Koordinat titik yang akan dirotasi
+    point1 = a.GetX();
+    point2 = a.GetY();
+    point3 = a.GetZ();
+
+    float center1, center2, center3;
+    // Koordinat sumbu
+    center1 = center.GetX();
+    center2 = center.GetY();
+    center3 = center.GetZ();
+
+    float tempx, tempy, tempz;
+    // Sumbu x
+    if(center1==1){
+        tempx = point1;
+        tempy = ( point2 * (round(cos( degree * PHI / 180.0 )*100)/100) ) - ( point3 * (round(sin( degree * PHI / 180.0 )*100)/100) );
+        tempz = ( point2 * (round(sin( degree * PHI / 180.0 )*100)/100) ) + ( point3 * (round(cos( degree * PHI / 180.0 )*100)/100) );
+    }
+    // Sumbu y
+    else if(center2==1){
+        tempx = ( point3 * (round(sin( degree * PHI / 180.0 )*100)/100) ) + ( point1 * (round(cos( degree * PHI / 180.0 )*100)/100) );
+        tempy = point2;
+        tempz = ( point3 * (round(cos( degree * PHI / 180.0 )*100)/100) ) - ( point1 * (round(sin( degree * PHI / 180.0 )*100)/100) );
+    }
+    // Sumbu z
+    else{
+        tempx = ( point1 * (round(cos( degree * PHI / 180.0 )*100)/100) ) - ( point2 * (round(sin( degree * PHI / 180.0 )*100)/100) );
+        tempy = ( point1 * (round(sin( degree * PHI / 180.0 )*100)/100) ) + ( point2 * (round(cos( degree * PHI / 180.0 )*100)/100) );
+        tempz = point3;
+    }
+
+    Vector res(tempx, tempy, tempz);
+    return res;
+}
+
 float GetNFovy(float fov){
     return tan((fov/2)*PHI/180);
 }
@@ -160,7 +218,7 @@ Ray ComputeRay(Camera cam, Matrix norm, float a, float b){
 
 Ray RayThruPixel(Camera cam, int i, int j, float width, float height){
 	Ray temp;
-	
+
 	Matrix norm = NormalizeUVW(cam);
 	float fovy=cam.Getfovy();
 	float Nfovy=GetNFovy(fovy);
